80f2014e6b59a8522c3a5dfde984023a0c3f0d0c
yman0784 <tykesam@yahoo.co.jp>
2023-08-16 12:09:47 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      flash[:notice] = "loginしました" 
       reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 
