7ac80371a8698892b66cbddfd3030bb62a0e62d3
yman0784 <tykesam@yahoo.co.jp>
2023-08-18 23:31:09 +0900

    correct user_model activated_at in_active

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,7 @@ class User < ApplicationRecord
 
   def activate
     update_attribute(:activated, true)
-    update_attribute(:activated, Time.zone.now)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
   def send_activation_email
